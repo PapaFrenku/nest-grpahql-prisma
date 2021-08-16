af699435a5873aead0d22ca266babb0510ac4770
Илья Овчинников <nerdik5@gmail.com>
2021-08-16 10:58:21 +0300

    s

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
